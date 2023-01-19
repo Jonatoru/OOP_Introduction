7d38f40177d3940b3ae147f85f3d5c0c74e4fe9b
Jonatoru <jetrence95@gmail.com>
2023-01-19 23:16:52 +0500

    Решил задачу 1.

---
+++ src/transport/Car.java
@@ -0,0 +1,120 @@
+package transport;
+
+public class Car {
+    private final String brand;
+    private final String model;
+    private double engineVolume;
+    private String color;
+    private final int year;
+    private final String country;
+    private String transmission;
+    private final String bodyType;
+    private String registrationNumber;
+    private final int numberOfSeats;
+    private boolean summerTires;
+
+    /** Создаю конструктор для автомобилей
+     *
+     * @param brand - марка
+     * @param model - модель
+     * @param engineVolume - объём двигателя
+     * @param color - цвет
+     * @param year - год
+     * @param country - страна проиводитель
+     * @param transmission - Коробка передач
+     * @param bodyType - Тип кузова
+     * @param registrationNumber - Регистрационный номер
+     * @param numberOfSeats - Количество мест
+     * @param summerTires - признак Летняя или Зимняя резина
+     */
+    public Car (String brand, String model, double engineVolume, String color, int year, String country, String transmission, String bodyType, String registrationNumber, int numberOfSeats, boolean summerTires){
+        this.brand = brand == null || brand.isBlank() ? "default" : brand;
+        this.model = model == null || model.isBlank() ? "default" : model;
+        this.engineVolume = engineVolume <= 0 ? 1.5 : engineVolume;
+        this.color = color == null || color.isBlank() ? "белый" : color;
+        this.year = year <= 0 ? 2000 : year;
+        this.country = country == null || country.isBlank() ? "default" : country;
+        this.transmission = transmission == null || transmission.isBlank() ? "default" : transmission;
+        this.bodyType = bodyType == null || bodyType.isBlank() ? "default" : bodyType;
+        this.registrationNumber = registrationNumber == null || registrationNumber.isBlank() ? "А000АА00" : registrationNumber;
+        this.numberOfSeats = numberOfSeats <= 0 ? 1 : numberOfSeats;
+        this.summerTires = summerTires;
+    }
+
+    public String getBrand(){
+        return this.brand;
+    }
+
+    public String getModel(){
+        return  this.model;
+    }
+
+    public double getEngineVolume(){
+        return this.engineVolume;
+    }
+
+    public String getColor(){
+        return this.color;
+    }
+
+    public int getYear(){
+        return this.year;
+    }
+
+    public String getCountry(){
+        return  this.country;
+    }
+
+    public String getBodyType() {
+        return bodyType;
+    }
+
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public String getTransmission() {
+        return transmission;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public boolean isSummerTires() {
+        return summerTires;
+    }
+
+    public void setEngineVolume(double engineVolume) {
+        this.engineVolume = engineVolume;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setTransmission(String transmission) {
+        this.transmission = transmission;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public void setSummerTires(boolean summerTires) {
+        this.summerTires = summerTires;
+    }
+    public void changeTiresToSeasonal (int currentMonth) {
+        if (currentMonth == 1 || currentMonth == 2 || currentMonth == 12){
+            setSummerTires(false);
+        } else setSummerTires(true);
+    }
+
+    /** Переписал toString под ноав данные
+     *
+     * @return было brand+" "+ model+ ", год выпуска: "+ year+ ", страна производства: "+ country+ ", цвет: "+ color+ ", объём двигателя: "+ engineVolume+ ".";
+     */
+    public String toString(){
+        return getBrand()+" "+ getModel()+ ", год выпуска: "+ getYear()+ ", страна производства: "+ getCountry()+ ", цвет: "+ getColor()+ ", объём двигателя: "+ getEngineVolume()+ ", коробка передач: "+ getTransmission()+ " , тип кузова: " + getBodyType()+ " , регистрационный номер: "+ getRegistrationNumber()+ " , количество мест: "+ getNumberOfSeats()+ " , признак Летняя резина: "+ isSummerTires()+ ".";
+    }
+}
